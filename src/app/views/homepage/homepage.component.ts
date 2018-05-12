a314a022bec844397362f267672e6ab77a9b4bdc
Sourav <souravrkl.gamefreak@gmail.com>
2018-05-12 15:32:21 +0530

    [ADD] grid, card components in nested route

--- src/app/views/homepage/homepage.component.ts
+++ src/app/views/homepage/homepage.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  public dogs: Array<number> = [1, 2, 3, 4];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatCardModule, MatGridListModule, MatButtonModule } from '@angular/material';
+
 import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   declarations: [
     HomepageComponent
